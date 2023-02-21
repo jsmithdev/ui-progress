4ca73f2dd26efa6e209adfde2e7b72417347e58e
Jamie Smith <me@jsmith.dev>
2023-02-21 16:34:32 -0500

    add new type (pill), textColor & textSize apis

--- force-app/main/default/lwc/uiProgress/uiProgress.js
+++ force-app/main/default/lwc/uiProgress/uiProgress.js
@@ -1,17 +1,20 @@
 import { api, LightningElement } from 'lwc';
 
-import {Progress} from './progress';
+import './progress';
 
+import {
+	getOptions,
+} from './barOptions.js';
 
 export default class UiProgress extends LightningElement {
-
-	isCustom = false;
 
 	@api text;
 	@api type = 'line';
 	@api width = '200px';
 	@api color = '#1d07e8';
 	@api background = '#EEE';
+	@api textColor = '#FFF';
+	@api textSize = '1rem'
 	@api customSvg = '';
 
 	@api
@@ -25,23 +28,28 @@ export default class UiProgress extends LightningElement {
 		this._progress = Math.floor(value);
 
 		if (this.progressBar) {
-			this.progressBar.animate(value / 100);
+			this.progressBar.animate(this._progress / 100);
 		}
 	}
 
 	get options() {
-		return this.getOptions(this.type);
-	}
-
-	connectedCallback() {
-		if (this.type === 'custom') this.isCustom = true;
+		return getOptions({
+			text: this.text.length > 0,
+			type: this.type,
+			color: this.color,
+			background: this.background,
+			text: {
+				color: this.textColor,
+				size: this.textSize,
+			}
+		})
 	}
 
 	renderedCallback() {
 
 		const el = this.template.querySelector('.container')
 
-		el.style.width = this.width;
+		//el.style.width = this.width;
 
 		if (this.type === 'circle') {
 			this.progressBar = new ProgressBar.Circle(el, this.options);
@@ -62,129 +70,14 @@ export default class UiProgress extends LightningElement {
 
 			this.progressBar.set(0);
 		}
+		else if(this.type === 'pill'){
+			this.progressBar = new ProgressBar.Line(el, this.options);
+		}
 		else {
+			// line is default
 			this.progressBar = new ProgressBar.Line(el, this.options);
 		}
 
 		this.progressBar.animate(this.progress / 100);
-	}
-
-	getOptions(opt) {
-
-		if (opt === 'circle') {
-			return this.circleOptions()
-		}
-
-		if (this.type === 'semi') {
-			return this.semiOptions();
-		}
-		
-		const opts = {}
-
-		if (this.text) {
-			opts.text = this.textOptions();
-		}
-
-		return { ...this.lineOptions(), ...opts }
-	}
-
-	textOptions() {
-		return {
-			style: {
-				// Text color.
-				// Default: same as stroke color (options.color)
-				//color: '#999',
-				position: 'absolute',
-				right: '0',
-				top: '30px',
-				padding: 0,
-				margin: 0,
-				transform: null
-			},
-			autoStyleContainer: false
-		}
-	}
-
-	lineOptions() {
-		return {
-			strokeWidth: 4,
-			easing: 'easeInOut',
-			duration: 1400,
-			color: this.color,
-			trailColor: this.background,
-			trailWidth: 1,
-			svgStyle: { width: '100%', height: '100%' },
-			from: { color: this.color },
-			to: { color: this.color },
-			step: (state, bar) => {
-				bar.path.setAttribute('stroke', state.color);
-				if (this.text) {
-					bar.setText(Math.round(bar.value() * 100) + ' %');
-				}
-			}
-		}
-	}
-
-	circleOptions() {
-		return {
-			color: this.color,
-			trailColor: this.background,
-			// This has to be the same size as the maximum width to
-			// prevent clipping
-			strokeWidth: 4,
-			trailWidth: 1,
-			easing: 'easeInOut',
-			duration: 1400,
-			text: {
-				autoStyleContainer: false,
-			},
-			from: { color: this.color, width: 1 },
-			to: { color: this.color, width: 4 },
-			// Set default step function for all animate calls
-			step: function (state, circle) {
-
-				circle.path.setAttribute('stroke', state.color);
-				circle.path.setAttribute('stroke-width', state.width);
-
-				const value = Math.round(circle.value() * 100);
-
-				if (value === 0) {
-					circle.setText('');
-				}
-				else {
-					circle.setText(value);
-				}
-			}
-		}
-	}
-
-	semiOptions() {
-		return {
-			strokeWidth: 6,
-			color: this.color,
-			trailColor: this.background,
-			trailWidth: 1,
-			easing: 'easeInOut',
-			duration: 1400,
-			svgStyle: null,
-			text: {
-				value: '',
-				alignToBottom: false
-			},
-			from: { color: this.color },
-			to: { color: this.color },
-			// Set default step function for all animate calls
-			step: (state, bar) => {
-				bar.path.setAttribute('stroke', state.color);
-				const value = Math.round(bar.value() * 100);
-				if (value === 0) {
-					bar.setText('');
-				} else {
-					bar.setText(value);
-				}
-
-				bar.text.style.color = state.color;
-			}
-		}
 	}
 }
